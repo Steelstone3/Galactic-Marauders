b10cb679c6f3924d666c7126e12f609510c99be4
Skippy <steelstone3@gmail.com>
2023-10-10 01:21:25 +0100

    Version 0.1.8

--- src/systems/player_weapon_select.rs
+++ src/systems/player_weapon_select.rs
@@ -2,7 +2,10 @@ use bevy::prelude::{info, Input, KeyCode, Res, ResMut};
 
 use crate::resources::selected_weapon::SelectedWeapon;
 
-pub fn player_weapon_select(input: Res<Input<KeyCode>>, mut selected_weapon: ResMut<SelectedWeapon>) {
+pub fn player_weapon_select(
+    input: Res<Input<KeyCode>>,
+    mut selected_weapon: ResMut<SelectedWeapon>,
+) {
     // Weapon 1
     if input.just_pressed(KeyCode::Key1) {
         selected_weapon.0 = 1;
